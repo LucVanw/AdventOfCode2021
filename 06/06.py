9d093d0ca52a9ec0ff12175c43a92e4e9cb8250e
Luc <luc@alkivi.fr>
2021-12-07 20:35:02 +0100

    day6

---
+++ 06/06.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+def format_file(rows):
+    commands = []
+    for row in rows:
+        row = row.split(",")
+        for command in row:
+            fish = int(command)
+            commands.append(fish)
+    return commands
+
+
+with open('input.txt') as f:
+    rows = f.readlines()
+    commands = format_file(rows)
+    print(f"Commands : {commands}")
+    sea = [0,0,0,0,0,0,0,0,0]
+
+    for command in commands:
+        sea[command] += 1
+
+    print(f"Sea : {sea}")
+
+    for day in range(80):
+        newsea = [0,0,0,0,0,0,0,0,0]
+        for index in range(9):
+            if index == 8:
+                newsea[8] = sea[0]
+                newsea[6] += sea[0] 
+            else:
+                newsea[index] = sea[index+1]
+        sea = newsea
+        print(f"sea : {sea}")
+    print(f"Result : {sum(sea)}")
